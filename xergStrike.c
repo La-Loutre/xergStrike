ac8f293a6cd4d65bd0a40509870b8eb7e579d776
Louis Douriez <ldouriez@gmail.com>
2014-04-20 15:33:55 +0200

    légers changement

--- xergStrike.c
+++ xergStrike.c
@@ -4,10 +4,17 @@
 #include <assert.h>
 #include "xergStrike.h"
 #include <string.h>
-
-static bool usage(int a ,char **b)
+static void afficherAide()
 {
-  assert(a>1&&a<6);
+  printf(MESSAGE_AIDE);
+  
+}
+static bool usage(int argc ,char **argv)
+{
+  if(argc!=4&&argc!=5){
+    afficherAide();
+    return false;
+  }
   return true;
 }
 static bool estMinuscule(char a)
@@ -23,14 +30,7 @@ static bool estLettre(char a)
 {
   return (estMajuscule(a)||estMinuscule(a))?true:false;
 }
-static void afficherAide()
-{
-  printf("\n\n\nAide de la fonction \n ");
-  printf("\nhelp: affichage de l'aide");
-  printf("\nmasque <masque> <texte> (sans les chevrons) : permet d'obtenir le résulta du masquage de texte par masque");
-  printf("\n\n");
- 
-}
+
 static void afficherMessageMasquageTexte(int nbrMasque)
 {
   printf("\n\n______________________\n\nMasquage de texte *%d\n______________________\n\n",nbrMasque>26?26:nbrMasque);
@@ -39,7 +39,7 @@ static void afficherMessageMasquageTexte(int nbrMasque)
 
 static void afficherResultatMasquageMultiple(char *masque,char *original,char *resultat,int rang){
 
-      printf("\nmasque : %20s  \ntexte : %21s \nresultat : %18s  (rang %d)\n_____________\n",masque,original,resultat,rang);
+  printf("\nmasque : %20s  \ntexte : %21s \nresultat : %18s  (rang %d)\n_____________\n",masque,original,resultat,rang);
 
 }
 
@@ -77,7 +77,7 @@ void masquage(char *masque,char *texte,char *final)
  
   for(int i=0,y=0;i<strlen(texte)+1;i++,y=y%strlen(masque)){
     if(estLettre(texte[i]))
-       final[i]=integerToAlpha(alphabetToInt(masque[y++])+alphabetToInt(texte[i])+1);
+      final[i]=integerToAlpha(alphabetToInt(masque[y++])+alphabetToInt(texte[i])+1);
     else
       final[i]=texte[i];
   }
@@ -109,7 +109,12 @@ void tabToTab(char *a,char *b)
 void affichageMasquageMultiple(int argc,char **argv)
 {
   int nbrMasque=atoi(argv[4]);
-  assert(nbrMasque>=1);
+
+  if(nbrMasque<1)
+    {
+      printf(ERREUR_NB_MAX_MASQUE);
+      assert(0);
+    }
   int tailleFinal=strlen(argv[3])+1;
   char *final=copierChaine(argv[3]);
   char save[tailleFinal];
@@ -127,7 +132,7 @@ void affichageMasquageMultiple(int argc,char **argv)
     }
 
   if(nbrMasque>26)
-      printf("\n\nAttention le nombre max de masquage est 26 \n(Car texte + x*26mod(26)=texte )\n\n");
+    printf(MESSAGE_NB_MAX_MASQUAGE);
   free(final);
    
 }
--- xergStrike.h
+++ xergStrike.h
@@ -1,8 +1,9 @@
 #ifndef XERG_STRIKE_H
 #define XERG_STRIKE_H
 #include <stdbool.h>
- 
-
+#define MESSAGE_AIDE "\n\nhelp: affichage de l'aide\nmasque <masque> <texte> (sans les chevrons) : permet d'obtenir le résulta du masquage de texte par masque\n\n"
+#define MESSAGE_NB_MAX_MASQUAGE "\n\nAttention le nombre max de masquage est 26 \n(Car texte + x*26mod(26)=texte )\n\n"
+#define ERREUR_NB_MAX_MASQUE "\n\nErreur : \nnombre de masque incorrect\n\n"
 void launcher(int,char **);
 void masquage(char *,char*,char*);
 
